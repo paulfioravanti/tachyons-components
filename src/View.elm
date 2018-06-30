38d1682ac56ddf77100c4f242f96d762a69ea32f
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-06-30 22:48:31 +1000

    Refactor main components into separate modules

---
+++ src/View.elm
@@ -0,0 +1,60 @@
+module View exposing (view)
+
+import Html exposing (Html, a, div, header, nav, small, text)
+import Html.Attributes exposing (class, href, title)
+import Model exposing (Model)
+import Msg exposing (Msg)
+import Styles
+
+
+view : Model -> Html Msg
+view model =
+    header [ class Styles.header ]
+        [ div [ class Styles.headerContent ]
+            [ div [ class Styles.title ]
+                [ a
+                    [ class Styles.titleLink
+                    , href "http://tachyons.io/"
+                    , title "Home"
+                    ]
+                    [ text "Tachyons "
+                    , div [ class Styles.versionNumberWrapper ]
+                        [ small [ class Styles.versionNumber ]
+                            [ text "v4.9.1" ]
+                        ]
+                    ]
+                ]
+            , nav [ class Styles.nav ]
+                [ a
+                    [ class Styles.navLink
+                    , href "http://tachyons.io/docs"
+                    , title "Documentation"
+                    ]
+                    [ text "Docs" ]
+                , a
+                    [ class Styles.navLink
+                    , href "#"
+                    , title "Components"
+                    ]
+                    [ text "Components" ]
+                , a
+                    [ class Styles.navLink
+                    , href "http://tachyons.io/gallery"
+                    , title "Gallery of sites built with Tachyons"
+                    ]
+                    [ text "Gallery" ]
+                , a
+                    [ class Styles.navLink
+                    , href "http://tachyons.io/resources"
+                    , title "Resources"
+                    ]
+                    [ text "Resources" ]
+                , a
+                    [ class Styles.navLink
+                    , href "http://github.com/tachyons-css/tachyons"
+                    , title "Tachyons on Github"
+                    ]
+                    [ text "Github" ]
+                ]
+            ]
+        ]
