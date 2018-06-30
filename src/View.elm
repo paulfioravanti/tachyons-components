9924543f51ebb6732a84707c618cb791f92311f0
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-06-30 22:57:53 +1000

    View refactoring

--- src/View.elm
+++ src/View.elm
@@ -9,52 +9,68 @@ import Styles
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ pageHeader ]
+
+
+pageHeader : Html Msg
+pageHeader =
     header [ class Styles.header ]
         [ div [ class Styles.headerContent ]
-            [ div [ class Styles.title ]
-                [ a
-                    [ class Styles.titleLink
-                    , href "http://tachyons.io/"
-                    , title "Home"
-                    ]
-                    [ text "Tachyons "
-                    , div [ class Styles.versionNumberWrapper ]
-                        [ small [ class Styles.versionNumber ]
-                            [ text "v4.9.1" ]
-                        ]
-                    ]
-                ]
-            , nav [ class Styles.nav ]
-                [ a
-                    [ class Styles.navLink
-                    , href "http://tachyons.io/docs"
-                    , title "Documentation"
-                    ]
-                    [ text "Docs" ]
-                , a
-                    [ class Styles.navLink
-                    , href "#"
-                    , title "Components"
-                    ]
-                    [ text "Components" ]
-                , a
-                    [ class Styles.navLink
-                    , href "http://tachyons.io/gallery"
-                    , title "Gallery of sites built with Tachyons"
-                    ]
-                    [ text "Gallery" ]
-                , a
-                    [ class Styles.navLink
-                    , href "http://tachyons.io/resources"
-                    , title "Resources"
-                    ]
-                    [ text "Resources" ]
-                , a
-                    [ class Styles.navLink
-                    , href "http://github.com/tachyons-css/tachyons"
-                    , title "Tachyons on Github"
-                    ]
-                    [ text "Github" ]
+            [ pageTitle
+            , pageHeaderNav
+            ]
+        ]
+
+
+pageTitle : Html Msg
+pageTitle =
+    div [ class Styles.title ]
+        [ a
+            [ class Styles.titleLink
+            , href "http://tachyons.io/"
+            , title "Home"
+            ]
+            [ text "Tachyons "
+            , div [ class Styles.versionNumberWrapper ]
+                [ small [ class Styles.versionNumber ]
+                    [ text "v4.9.1" ]
                 ]
             ]
+        ]
+
+
+pageHeaderNav : Html Msg
+pageHeaderNav =
+    nav [ class Styles.nav ]
+        [ a
+            [ class Styles.navLink
+            , href "http://tachyons.io/docs"
+            , title "Documentation"
+            ]
+            [ text "Docs" ]
+        , a
+            [ class Styles.navLink
+            , href "#"
+            , title "Components"
+            ]
+            [ text "Components" ]
+        , a
+            [ class Styles.navLink
+            , href "http://tachyons.io/gallery"
+            , title "Gallery of sites built with Tachyons"
+            ]
+            [ text "Gallery" ]
+        , a
+            [ class Styles.navLink
+            , href "http://tachyons.io/resources"
+            , title "Resources"
+            ]
+            [ text "Resources" ]
+        , a
+            [ class Styles.navLink
+            , href "http://github.com/tachyons-css/tachyons"
+            , title "Tachyons on Github"
+            ]
+            [ text "Github" ]
         ]
