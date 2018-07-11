3faeb2a80768d1be290ba85514c539636acbe9e8
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-11 22:08:53 +1000

    Add 404 error page

--- src/ErrorPage/FourOhFour/Styles.elm
+++ src/ErrorPage/FourOhFour/Styles.elm
@@ -1,11 +1,91 @@
 module ErrorPage.FourOhFour.Styles
     exposing
-        ( container
+        ( callToAction
+        , header
+        , heading
+        , link
+        , linkList
+        , linkListElement
+        , section
+        , subheading
         )
 
 
-container : String
-container =
-    [ ""
+callToAction : String
+callToAction =
+    [ "f4 f3-l"
+    , "fw1"
+    , "i"
+    , "mt4 mt5-l"
+    , "tc"
+    ]
+        |> String.join " "
+
+
+header : String
+header =
+    [ "lh-copy"
+    , "ph5"
+    , "tc"
+    ]
+        |> String.join " "
+
+
+heading : String
+heading =
+    [ "code"
+    , "dib"
+    , "f1 f-headline-l"
+    , "fw9"
+    , "light-purple"
+    , "mb3"
+    , "tracked-tight"
+    ]
+        |> String.join " "
+
+
+link : String
+link =
+    [ "black"
+    , "db"
+    , "f5 f4-ns"
+    , "hover-light-purple"
+    , "link"
+    , "ph3"
+    , "pv2"
+    ]
+        |> String.join " "
+
+
+linkList : String
+linkList =
+    [ "list"
+    , "mt5"
+    , "pl0"
+    , "tc"
+    , "w-100"
+    ]
+        |> String.join " "
+
+
+linkListElement : String
+linkListElement =
+    "dib"
+
+
+section : String
+section =
+    [ "baskerville"
+    , "bg-washed-blue"
+    , "vh-100"
+    ]
+        |> String.join " "
+
+
+subheading : String
+subheading =
+    [ "f1-l"
+    , "fw1"
+    , "tc"
     ]
         |> String.join " "
--- src/ErrorPage/FourOhFour/View.elm
+++ src/ErrorPage/FourOhFour/View.elm
@@ -1,10 +1,51 @@
 module ErrorPage.FourOhFour.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import ErrorPage.FourOhFour.Styles as Styles
+import Html exposing (Html, a, div, h1, h2, header, li, p, section, text, ul)
+import Html.Attributes exposing (attribute, class, href)
 
 
 view : Html msg
 view =
     div [ attribute "data-name" "component" ]
-        []
+        [ section [ class Styles.section ]
+            [ heading
+            , p [ class Styles.callToAction ]
+                [ text "Are you looking for one of these?" ]
+            , links
+            ]
+        ]
+
+
+heading : Html msg
+heading =
+    header [ class Styles.header ]
+        [ h1 [ class Styles.heading ]
+            [ text "404" ]
+        , h2 [ class Styles.subheading ]
+            [ text "Sorry, we can't find the page you are looking for." ]
+        ]
+
+
+links : Html msg
+links =
+    let
+        linkList =
+            [ ( "/", "Home" )
+            , ( "/about", "About" )
+            , ( "/careers", "Careers" )
+            , ( "/contact", "Contact" )
+            , ( "/signup", "Sign Up" )
+            , ( "/help", "Help" )
+            ]
+    in
+        ul [ class Styles.linkList ]
+            (List.map linkListElement linkList)
+
+
+linkListElement : ( String, String ) -> Html msg
+linkListElement ( url, linkText ) =
+    li [ class Styles.linkListElement ]
+        [ a [ class Styles.link, href url ]
+            [ text linkText ]
+        ]
