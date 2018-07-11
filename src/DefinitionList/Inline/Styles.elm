b02e67a9918b140c7a656b52a0df395b37d8b4d0
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-11 19:48:06 +1000

    Add the first definition list page'

--- src/DefinitionList/Inline/Styles.elm
+++ src/DefinitionList/Inline/Styles.elm
@@ -1,11 +1,47 @@
 module DefinitionList.Inline.Styles
     exposing
-        ( container
+        ( description
+        , descriptionList
+        , glossary
+        , heading
+        , term
         )
 
 
-container : String
-container =
-    [ ""
+description : String
+description =
+    [ "dib"
+    , "ml1"
+    , "gray"
+    ]
+        |> String.join " "
+
+
+descriptionList : String
+descriptionList =
+    [ "f6"
+    , "lh-title"
+    , "mv2"
+    ]
+        |> String.join " "
+
+
+glossary : String
+glossary =
+    "pa3 pa5-ns"
+
+
+heading : String
+heading =
+    [ "f6"
+    , "fw6"
+    ]
+        |> String.join " "
+
+
+term : String
+term =
+    [ "b"
+    , "dib"
     ]
         |> String.join " "
--- src/DefinitionList/Inline/View.elm
+++ src/DefinitionList/Inline/View.elm
@@ -1,10 +1,40 @@
 module DefinitionList.Inline.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import DefinitionList.Inline.Styles as Styles
+import Html exposing (Html, div, dd, dl, dt, h4, text)
+import Html.Attributes exposing (attribute, class)
 
 
 view : Html msg
 view =
-    div [ attribute "data-name" "component" ]
-        []
+    let
+        descriptionLists =
+            [ ( "W", "Wins" )
+            , ( "L", "Losses" )
+            , ( "PCT", "Winning Percentages" )
+            , ( "GB", "Games Back" )
+            , ( "Home", "Home Record" )
+            , ( "Road", "Road Record" )
+            , ( "Div", "Division Record" )
+            ]
+    in
+        div [ attribute "data-name" "component" ]
+            [ div [ class Styles.glossary ]
+                (heading :: List.map descriptionList descriptionLists)
+            ]
+
+
+heading : Html msg
+heading =
+    h4 [ class Styles.heading ]
+        [ text "Glossary" ]
+
+
+descriptionList : ( String, String ) -> Html msg
+descriptionList ( term, description ) =
+    dl [ class Styles.descriptionList ]
+        [ dt [ class Styles.term ]
+            [ text (term ++ ":") ]
+        , dd [ class Styles.description ]
+            [ text description ]
+        ]
