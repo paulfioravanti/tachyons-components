1300dfab96ac51322aca3b6d93138b2c90c33d60
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 09:43:42 +1000

    Add form checkbox list page

--- src/Form/CheckboxList/Styles.elm
+++ src/Form/CheckboxList/Styles.elm
@@ -1,11 +1,46 @@
 module Form.CheckboxList.Styles
     exposing
-        ( container
+        ( checkbox
+        , checkboxWrapper
+        , fieldset
+        , form
+        , label
+        , legend
         )
 
 
-container : String
-container =
-    [ ""
+checkbox : String
+checkbox =
+    "mr2"
+
+
+checkboxWrapper : String
+checkboxWrapper =
+    [ "flex"
+    , "items-center"
+    , "mb2"
+    ]
+        |> String.join " "
+
+
+fieldset : String
+fieldset =
+    "bn"
+
+
+form : String
+form =
+    "pa4"
+
+
+label : String
+label =
+    "lh-copy"
+
+
+legend : String
+legend =
+    [ "fw7"
+    , "mb2"
     ]
         |> String.join " "
--- src/Form/CheckboxList/View.elm
+++ src/Form/CheckboxList/View.elm
@@ -1,10 +1,48 @@
 module Form.CheckboxList.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Form.CheckboxList.Styles as Styles
+import Html exposing (Html, div, fieldset, form, input, label, legend, text)
+import Html.Attributes exposing (attribute, class, for, id, type_, value)
 
 
 view : Html msg
 view =
     div [ attribute "data-name" "component" ]
-        []
+        [ form [ class Styles.form ]
+            [ fieldset [ id "favorite_movies", class Styles.fieldset ]
+                (heading :: List.map checkbox checkboxes)
+            ]
+        ]
+
+
+heading : Html msg
+heading =
+    legend [ class Styles.legend ]
+        [ text "Favorite Movies" ]
+
+
+checkbox : ( String, String ) -> Html msg
+checkbox ( checkboxId, name ) =
+    div [ class Styles.checkboxWrapper ]
+        [ input
+            [ class Styles.checkbox
+            , type_ "checkbox"
+            , id checkboxId
+            , value checkboxId
+            ]
+            []
+        , label [ class Styles.label, for checkboxId ]
+            [ text name ]
+        ]
+
+
+checkboxes : List ( String, String )
+checkboxes =
+    [ ( "spacejam", "Space Jam" )
+    , ( "airbud", "Air Bud" )
+    , ( "hocuspocus", "Hocus Pocus" )
+    , ( "diehard", "Die Hard" )
+    , ( "primer", "Primer" )
+    , ( "proxy", "Hudsucker Proxy" )
+    , ( "homealone", "Home Alone" )
+    ]
