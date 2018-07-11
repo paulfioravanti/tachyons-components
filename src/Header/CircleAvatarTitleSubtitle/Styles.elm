780a9e7ff2753d585b05fc27111374dc39f7b3df
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 09:57:24 +1000

    Add circle avatar title subtitle page

--- src/Header/CircleAvatarTitleSubtitle/Styles.elm
+++ src/Header/CircleAvatarTitleSubtitle/Styles.elm
@@ -1,11 +1,47 @@
 module Header.CircleAvatarTitleSubtitle.Styles
     exposing
-        ( container
+        ( header
+        , heading
+        , image
+        , subheading
         )
 
 
-container : String
-container =
-    [ ""
+header : String
+header =
+    [ "pv4 pv5-ns"
+    , "tc"
+    ]
+        |> String.join " "
+
+
+heading : String
+heading =
+    [ "f5 f4-ns"
+    , "fw6"
+    , "mid-gray"
+    ]
+        |> String.join " "
+
+
+image : String
+image =
+    [ "b--black-10"
+    , "ba"
+    , "br-100"
+    , "h3"
+    , "pa1"
+    , "w3"
+    ]
+        |> String.join " "
+
+
+subheading : String
+subheading =
+    [ "f6"
+    , "fw2"
+    , "gray"
+    , "tracked"
+    , "ttu"
     ]
         |> String.join " "
--- src/Header/CircleAvatarTitleSubtitle/View.elm
+++ src/Header/CircleAvatarTitleSubtitle/View.elm
@@ -1,10 +1,23 @@
 module Header.CircleAvatarTitleSubtitle.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Header.CircleAvatarTitleSubtitle.Styles as Styles
+import Html exposing (Html, div, h1, h2, header, img, text)
+import Html.Attributes exposing (alt, attribute, class, src)
 
 
 view : Html msg
 view =
     div [ attribute "data-name" "component" ]
-        []
+        [ header [ class Styles.header ]
+            [ img
+                [ class Styles.image
+                , src "http://tachyons.io/img/logo.jpg"
+                , alt "avatar"
+                ]
+                []
+            , h1 [ class Styles.heading ]
+                [ text "Jasper Whitehouse" ]
+            , h2 [ class Styles.subheading ]
+                [ text "Los Angeles" ]
+            ]
+        ]
