72759818513fb9ed25a41d93eafa016511f0aa5a
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 14:14:07 +1000

    Add left border quote page

--- src/Quote/LeftBorder/Styles.elm
+++ src/Quote/LeftBorder/Styles.elm
@@ -1,11 +1,46 @@
 module Quote.LeftBorder.Styles
     exposing
-        ( container
+        ( blockquote
+        , cite
+        , copy
+        , quote
         )
 
 
-container : String
-container =
-    [ ""
+blockquote : String
+blockquote =
+    [ "athelas"
+    , "b--blue"
+    , "bl"
+    , "bw2"
+    , "black-90"
+    , "ml0"
+    , "mt0"
+    , "pl4"
     ]
         |> String.join " "
+
+
+cite : String
+cite =
+    [ "f6"
+    , "fs-normal"
+    , "tracked"
+    , "ttu"
+    ]
+        |> String.join " "
+
+
+copy : String
+copy =
+    [ "f5 f4-m f3-l"
+    , "lh-copy"
+    , "measure"
+    , "mt0"
+    ]
+        |> String.join " "
+
+
+quote : String
+quote =
+    "pa4"
--- src/Quote/LeftBorder/View.elm
+++ src/Quote/LeftBorder/View.elm
@@ -1,10 +1,29 @@
 module Quote.LeftBorder.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Html exposing (Html, blockquote, cite, div, p, text)
+import Html.Attributes exposing (attribute, class)
+import Quote.LeftBorder.Styles as Styles
 
 
 view : Html msg
 view =
-    div [ attribute "data-name" "component" ]
-        []
+    let
+        copy =
+            """
+            Discipline in typography is a prime virtue. Individuality must be
+            secured by means that are rational. Distinction needs to be won by
+            simplicity and restraint. It is equally true that these qualities
+            need to be infused wiht a certain spirit and vitality, or they
+            degenerate into dullness and mediocrity.
+            """
+    in
+        div [ attribute "data-name" "component" ]
+            [ div [ class Styles.quote ]
+                [ blockquote [ class Styles.blockquote ]
+                    [ p [ class Styles.copy ]
+                        [ text copy ]
+                    , cite [ class Styles.cite ]
+                        [ text "―Stanley Morison" ]
+                    ]
+                ]
+            ]
