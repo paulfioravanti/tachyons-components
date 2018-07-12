28757726b5bdd8b0b0f1a86c08c5b2f3caa353c8
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 12:21:25 +1000

    Add list block item dotted border page

--- src/List_/BlockItemDottedBorder/Styles.elm
+++ src/List_/BlockItemDottedBorder/Styles.elm
@@ -1,11 +1,35 @@
 module List_.BlockItemDottedBorder.Styles
     exposing
-        ( container
+        ( list
+        , listItem
+        , listWrapper
         )
 
 
-container : String
-container =
-    [ ""
+list : String
+list =
+    [ "center"
+    , "list"
+    , "measure"
+    , "pl0"
     ]
         |> String.join " "
+
+
+listItem : String
+listItem =
+    [ "b--black-30"
+    , "b--dotted"
+    , "ba"
+    , "bl-0"
+    , "br-0"
+    , "bt-0"
+    , "lh-copy"
+    , "pv3"
+    ]
+        |> String.join " "
+
+
+listWrapper : String
+listWrapper =
+    "pa3 pa5-ns"
--- src/List_/BlockItemDottedBorder/View.elm
+++ src/List_/BlockItemDottedBorder/View.elm
@@ -1,10 +1,25 @@
 module List_.BlockItemDottedBorder.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Html exposing (Html, div, li, text, ul)
+import Html.Attributes exposing (attribute, class)
+import List_.BlockItemDottedBorder.Styles as Styles
 
 
 view : Html msg
 view =
-    div [ attribute "data-name" "component" ]
-        []
+    let
+        listItems =
+            [ "Orange", "Apple", "Peach", "Grape", "Grapefruit", "Kiwi" ]
+    in
+        div [ attribute "data-name" "component" ]
+            [ div [ class Styles.listWrapper ]
+                [ ul [ class Styles.list ]
+                    (List.map listItem listItems)
+                ]
+            ]
+
+
+listItem : String -> Html msg
+listItem itemName =
+    li [ class Styles.listItem ]
+        [ text itemName ]
