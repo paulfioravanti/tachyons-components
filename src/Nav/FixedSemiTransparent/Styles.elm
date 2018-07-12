a625143ee9dd273f5a2750857cab198cb04c53ad
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 13:06:29 +1000

    Add nav fixed semi transparent page

--- src/Nav/FixedSemiTransparent/Styles.elm
+++ src/Nav/FixedSemiTransparent/Styles.elm
@@ -1,11 +1,38 @@
 module Nav.FixedSemiTransparent.Styles
     exposing
-        ( container
+        ( header
+        , nav
+        , link
         )
 
 
-container : String
-container =
-    [ ""
+header : String
+header =
+    [ "bg-black-90"
+    , "fixed"
+    , "ph3 ph4-m ph5-l"
+    , "pv3 pv4-ns"
+    , "w-100"
+    ]
+        |> String.join " "
+
+
+link : String
+link =
+    [ "dib"
+    , "dim"
+    , "link"
+    , "mr3"
+    , "white"
+    ]
+        |> String.join " "
+
+
+nav : String
+nav =
+    [ "f6"
+    , "fw6"
+    , "tracked"
+    , "ttu"
     ]
         |> String.join " "
--- src/Nav/FixedSemiTransparent/View.elm
+++ src/Nav/FixedSemiTransparent/View.elm
@@ -1,10 +1,25 @@
 module Nav.FixedSemiTransparent.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Html exposing (Html, a, div, header, nav, text)
+import Html.Attributes exposing (attribute, class, href, title)
+import Nav.FixedSemiTransparent.Styles as Styles
 
 
 view : Html msg
 view =
-    div [ attribute "data-name" "component" ]
-        []
+    let
+        links =
+            [ "Home", "About", "Store", "Contact" ]
+    in
+        div [ attribute "data-name" "component" ]
+            [ header [ class Styles.header ]
+                [ nav [ class Styles.nav ]
+                    (List.map link links)
+                ]
+            ]
+
+
+link : String -> Html msg
+link name =
+    a [ href "#", title name, class Styles.link ]
+        [ text name ]
