7e0c2b08f3929d2b3c85691100693fa09a59f1d8
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-12 12:53:34 +1000

    Add marketing page

--- src/Marketing/IPhoneApp/Styles.elm
+++ src/Marketing/IPhoneApp/Styles.elm
@@ -1,11 +1,63 @@
 module Marketing.IPhoneApp.Styles
     exposing
-        ( container
+        ( copy
+        , heading
+        , image
+        , link
+        , section
+        , subheading
         )
 
 
-container : String
-container =
-    [ ""
+copy : String
+copy =
+    [ "ph3"
+    , "tc"
+    , "tl-ns"
+    ]
+        |> String.join " "
+
+
+heading : String
+heading =
+    [ "f3 f1-l"
+    , "fw2"
+    , "mb3"
+    , "mt4 mt0-ns"
+    ]
+        |> String.join " "
+
+
+image : String
+image =
+    [ "mw6"
+    , "ph5"
+    ]
+        |> String.join " "
+
+
+link : String
+link =
+    [ "dib"
+    , "grow"
+    ]
+        |> String.join " "
+
+
+section : String
+section =
+    [ "flex-ns"
+    , "items-center"
+    , "vh-100"
+    ]
+        |> String.join " "
+
+
+subheading : String
+subheading =
+    [ "f5 f3-l"
+    , "fw1"
+    , "mb4 mb5-l"
+    , "lh-title"
     ]
         |> String.join " "
--- src/Marketing/IPhoneApp/View.elm
+++ src/Marketing/IPhoneApp/View.elm
@@ -1,10 +1,24 @@
 module Marketing.IPhoneApp.View exposing (view)
 
-import Html exposing (Html, div)
-import Html.Attributes exposing (attribute)
+import Html exposing (Html, div, h1, h2, img, section, text)
+import Html.Attributes exposing (attribute, class, src)
+import Marketing.IPhoneApp.Logo as Logo
+import Marketing.IPhoneApp.Styles as Styles
 
 
 view : Html msg
 view =
     div [ attribute "data-name" "component" ]
-        []
+        [ section [ class Styles.section ]
+            [ div [ class Styles.image ]
+                [ img [ src "http://tachyons.io/img/iPhone7Vertical.jpg" ] [] ]
+            , div [ class Styles.copy ]
+                [ h1 [ class Styles.heading ]
+                    [ text "Connect in a whole new way." ]
+                , h2 [ class Styles.subheading ]
+                    [ text "Rated the #1 app for communicating with customers."
+                    ]
+                , Logo.iTunesStore
+                ]
+            ]
+        ]
