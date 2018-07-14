fe50d422cc7ea77a5a117ae28a3328305af926d9
Paul Fioravanti <paul.fioravanti@gmail.com>
2018-07-14 20:19:31 +1000

    Fix links

--- src/Collection/Movies/View.elm
+++ src/Collection/Movies/View.elm
@@ -2,8 +2,8 @@ module Collection.Movies.View exposing (view)
 
 import Collection.Movies.Styles as Styles
 import Collection.Movies.Movie as Movie exposing (Movie)
-import Html exposing (Html, a, article, div, dd, dl, dt, h2, img, text)
-import Html.Attributes exposing (alt, attribute, class, href, src, style)
+import Html exposing (Html, a, article, div)
+import Html.Attributes exposing (attribute, class, href, style)
 
 
 view : Html msg
@@ -20,7 +20,7 @@ view =
 
 movieLink : Movie -> Html msg
 movieLink { imageUrl, iTunesUrl, label } =
-    a [ class Styles.link ]
+    a [ class Styles.link, href iTunesUrl ]
         [ div
             [ class Styles.image
             , attribute "role" "img"
